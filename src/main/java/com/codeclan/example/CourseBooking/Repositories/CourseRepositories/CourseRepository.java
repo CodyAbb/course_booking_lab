9d1bbf074a52025c8381aa3e1e82b39bb7cd1d94
Cody Abbott <cody.abbott16@gmail.com>
2020-02-27 13:05:02 +0000

    implemented route to find course by star rating

--- src/main/java/com/codeclan/example/CourseBooking/Repositories/CourseRepositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/CourseBooking/Repositories/CourseRepositories/CourseRepository.java
@@ -4,6 +4,9 @@ import com.codeclan.example.CourseBooking.Models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByStarRating(int starRating);
 }
