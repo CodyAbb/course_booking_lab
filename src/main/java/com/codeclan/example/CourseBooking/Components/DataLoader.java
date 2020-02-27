ad71676c1d743cd71b0bbc6c26cbcf90834f4203
Cody Abbott <cody.abbott16@gmail.com>
2020-02-27 11:37:10 +0000

    setup dataloader with some mock database data

--- src/main/java/com/codeclan/example/CourseBooking/Components/DataLoader.java
+++ src/main/java/com/codeclan/example/CourseBooking/Components/DataLoader.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.CourseBooking.Components;
 
 import com.codeclan.example.CourseBooking.Models.Booking;
+import com.codeclan.example.CourseBooking.Models.Course;
+import com.codeclan.example.CourseBooking.Models.Customer;
 import com.codeclan.example.CourseBooking.Repositories.BookingRepositories.BookingRepository;
 import com.codeclan.example.CourseBooking.Repositories.CourseRepositories.CourseRepository;
 import com.codeclan.example.CourseBooking.Repositories.CustomerRepositories.CustomerRepository;
@@ -18,7 +20,32 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     CustomerRepository customerRepository;
 
-    public void run(ApplicationArguments args){
+    public DataLoader(){
 
+    }
+
+    public void run(ApplicationArguments args){
+        Course python = new Course("Intro to Python", "Inverness", 3);
+        courseRepository.save(python);
+
+        Course softwareDevelopment = new Course("Software Development", "Edinburgh", 5);
+        courseRepository.save(softwareDevelopment);
+
+        Customer cody = new Customer("Cody Abbott", "Edinburgh", 25);
+        customerRepository.save(cody);
+
+        Customer billy = new Customer("Billy McBilly", "Aberdeen", 21);
+        customerRepository.save(billy);
+
+        Customer rachel = new Customer("Rachel Davis", "Whitburn", 26);
+
+        Booking billyBooking = new Booking("23-02-2019", python, billy);
+        bookingRepository.save(billyBooking);
+
+        Booking codyBooking = new Booking("14-02-2020", softwareDevelopment, cody);
+        bookingRepository.save(codyBooking);
+
+        Booking rachelBooking = new Booking("21-02-2020", softwareDevelopment, rachel);
+        bookingRepository.save(rachelBooking);
     }
 }
